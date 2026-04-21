3142351074acd1814523df5b11a7e545df45c95d
lana-bu <lanabu@umich.edu>
2026-04-21 07:46:31 -0400

    auth form setup

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,11 +2,31 @@ import { Link } from 'react-router-dom';
 
 function Login() {
     return (
-        <div>
-            <main className='content-container'>
-                hello
-            </main>
-        </div>
+        <>
+            <h1 className="content-header">Log In</h1>
+
+            <div className='card'>
+                <form method="post">
+                    <div className='field'>
+                        <label for='usernameOrEmail'>Username/Email: </label>
+                        <input name='usernameOrEmail' placeholder='Enter username or email...' value=''/>
+                    </div>
+                    <div className='field'>
+                        <label for='password'>Password: </label>
+                        <input name='password' placeholder='Enter password...' value=''/>
+                    </div>
+                    
+                    <button className="btn" type="submit">Create Account</button>
+                </form>
+            </div>
+
+            <p class="register-links">
+                If you don't already have an account, register for one <Link to='/signup'>here</Link>.
+                <br>
+                </br>
+                (Or <Link to='/'>browse notes as Guest</Link>)
+            </p>
+        </>
     );
 }
 
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -2,11 +2,38 @@ import { Link } from 'react-router-dom';
 
 function Signup() {
     return (
-        <div>
-            <main className='content-container'>
-                hello
-            </main>
-        </div>
+        <>
+            <h1 className="content-header">Sign Up</h1>
+
+            <div className='card'>
+                <form method="post">
+                    <div className='field'>
+                        <label for='username'>Username: </label>
+                        <input name='username' placeholder='Create username...' value=''/>
+                    </div>
+                    <div className='field'>
+                        <label for='email'>Email: </label>
+                        <input name='username' placeholder='Enter email...' value=''/>
+                    </div>
+                    <div className='field'>
+                        <label for='password'>Password: </label>
+                        <input name='password' placeholder='Create password...' value=''/>
+                    </div>
+                    <div className='field'>
+                        <label for='password-confirm'>Confirm Password: </label>
+                        <input name='password-confirm' placeholder='Confirm password...' value=''/>
+                    </div>
+                    
+                    <button className="btn" type="submit">Create Account</button>
+                </form>
+            </div>
+
+        <p class="register-links">
+            If you already have an account, log in <Link to='/login'>here</Link>.
+            <br></br>
+            (Or <Link to='/'>browse notes as Guest</Link>)
+        </p>
+        </>
     );
 }
 
